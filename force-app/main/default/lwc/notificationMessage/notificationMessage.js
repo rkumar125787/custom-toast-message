4fbcfbcac3e58abfe9fc982a621cf33487f65458
Rajesh Kumar <rkumar125787@gmail.com>
2021-05-18 00:44:34 +0530

    fixed close unclose issue

--- force-app/main/default/lwc/notificationMessage/notificationMessage.js
+++ force-app/main/default/lwc/notificationMessage/notificationMessage.js
@@ -1,32 +1,37 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
 var timeout = null;
 export default class NotificationMessage extends LightningElement {
-    disabled = false;
-    timeDelay;
-    mode = 'sticky';
-    type;
-    message = 'Please not this is sample model for updating record we are discovering more on theprocess';
-    messageHeader = 'Record updated successfully.'
+    @api disabled = false;
+    @api timeDelay;
+    @api mode = 'dismiss';
+    @api type = 'info';
+    @api message = 'Sample Information Message';
+    @api messageHeader = 'Information'
     messageLink;
     closeToast() {
         this.disabled = true;
     }
-    connectedCallback() {
-        this.clearTostMessage();
-    }
     renderedCallback() {
-        if (this.type) {
 
-        }
-        //this.template.querySelector('.adc-notifications-toast-icon').classList.remove('adc-notifications-toast-icon--success');
     }
     clearTostMessage() {
         this.timeDelay = isNaN(parseInt(this.timeDelay)) ? 5000 : this.timeDelay;
         if (this.mode != 'sticky') {
             clearTimeout(timeout);
-            timeout = setTimeout(() => {
-                this.closeToast();
-            }, this.timeDelay);
+            timeout =
+                setTimeout(() => {
+                    this.closeToast();
+                }, this.timeDelay);
         }
+    }
+    @api showToast(mode, type, header, message, timedelay) {
+        this.disabled = false;
+        this.mode = mode || this.mode;
+        this.messageHeader = header || this.messageHeader;
+        this.message = message || this.message;
+        this.timeDelay = timedelay || this.timeDelay;
+        this.type = type || this.type;
+        console.log('mode:::' + this.mode);
+        this.clearTostMessage();
     }
 }
